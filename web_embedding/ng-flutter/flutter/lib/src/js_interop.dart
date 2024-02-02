34e2e67d0ce26ae4e24a6935280b92994c5d031d
Nick Mikhno <nikage23@gmail.com>
2024-02-02 19:33:57 +0300

    remove redundant boilerplates

--- web_embedding/ng-flutter/flutter/lib/src/js_interop.dart
+++
@@ -1,8 +0,0 @@
-/// Exposes useful functions to interop with JS from our Flutter app.
-library;
-
-export 'js_interop/counter_state_manager.dart';
-export 'js_interop/helper.dart' show broadcastAppEvent;
-
-// Replace with createJSInteropWrapper when updating to Dart 3.3.
-export 'dart:js_util' show createDartExport;
